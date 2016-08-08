7c23d8c9cb1a50e9eaecbcfbc87485c12db10147
David T. MacKenzie <Exit 0>
2016-08-07 21:08:55 -0600

    linting improvements to attribute generator

--- gen_attrs.py
+++ gen_attrs.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
+"""a 3d6 attr generator for mutant future"""
 
 import random
 
 def attrs():
+    """get a set of of MF attributes"""
 
     attributes = {}
 
-    attributes['Strength'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
-    attributes['Dexterity'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
-    attributes['Constitution'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
-    attributes['Intelligence'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
-    attributes['Willpower'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
-    attributes['Charisma'] = random.randrange(1, 7) + random.randrange(1, 7) + random.randrange(1, 7)
+    attributes['Strength'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                              random.randrange(1, 7))
+    attributes['Dexterity'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                               random.randrange(1, 7))
+    attributes['Constitution'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                                  random.randrange(1, 7))
+    attributes['Intelligence'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                                  random.randrange(1, 7))
+    attributes['Willpower'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                               random.randrange(1, 7))
+    attributes['Charisma'] = (random.randrange(1, 7) + random.randrange(1, 7) +
+                              random.randrange(1, 7))
 
     return attributes
 
 if __name__ == '__main__':
 
-    attributes = attrs()
+    ATTRIBUTES = attrs()
 
-    for key in attributes:
+    for key in ATTRIBUTES:
 
-        print(key, '=', attributes[key])
+        print(key, '=', ATTRIBUTES[key])
