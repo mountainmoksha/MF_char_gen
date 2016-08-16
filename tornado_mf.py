2208ceff5ad005031512d31772cee56970767663
David T. MacKenzie <Exit 0>
2016-08-15 20:09:50 -0600

    Standardized look and feel of character pages

--- tornado_mf.py
+++ tornado_mf.py
@@ -4,6 +4,93 @@ import tornado.ioloop
 import tornado.web
 import gen_char
 
+
+class ScreenFormatter():
+
+    def create_style_sheet(self):
+        """create style sheet for page"""
+
+        ret_str = '<html><head><style>'
+        ret_str = ret_str + 'header {'
+        ret_str = ret_str + '    background-color:blue;'
+        ret_str = ret_str + '    color:white;'
+        ret_str = ret_str + '    text-align:center;'
+        ret_str = ret_str + '    padding:5px;	 '
+        ret_str = ret_str + '}'
+        ret_str = ret_str + 'nav {'
+        ret_str = ret_str + '    line-height:20px;'
+        ret_str = ret_str + '    background-color:#eeeeee;'
+        ret_str = ret_str + '    float:left;'
+        ret_str = ret_str + '    height:100%;'
+        ret_str = ret_str + '    padding:5px;	      '
+        ret_str = ret_str + '}'
+        ret_str = ret_str + 'body_text {'
+        ret_str = ret_str + '    width:800px;'
+        ret_str = ret_str + '    float:left;'
+        ret_str = ret_str + '    padding:10px;	 	 '
+        ret_str = ret_str + '}'
+        ret_str = ret_str + 'footer {'
+        ret_str = ret_str + '    background-color:blue;'
+        ret_str = ret_str + '    color:white;'
+        ret_str = ret_str + '    clear:both;'
+        ret_str = ret_str + '    text-align:center;'
+        ret_str = ret_str + '    padding:5px;	 	 '
+        ret_str = ret_str + '}'
+        ret_str = ret_str + '</style>'
+        ret_str = ret_str + '<head></head>'
+        ret_str = ret_str + '<title>Mutant Future Character Generator</title>'
+        ret_str = ret_str + '</style></head>'
+
+        return ret_str
+
+
+    def create_nav(self):
+        """create navigation section"""
+
+        ret_str = '<nav>'
+        ret_str = ret_str + '<b>Create a character</b><br>'
+        ret_str = ret_str + '<b>Class:</b><br>'
+        ret_str = ret_str + '<form>'
+        ret_str = ret_str + '<select id="class_select">'
+        ret_str = ret_str + '<option value="BASIC_ANDROID">Basic Android</option>'
+        ret_str = ret_str + '<option value="SYNTHETIC_ANDROID">Synthetic Android</option>'
+        ret_str = ret_str + '<option value="REPLICANT">Replicant</option>'
+        ret_str = ret_str + '<option value="MUTANT_HUMAN">Mutant Human</option>'
+        ret_str = ret_str + '<option value="MUTANT_ANIMAL">Mutant Animal</option>'
+        ret_str = ret_str + '<option value="MUTANT_PLANT">Mutant Plant</option>'
+        ret_str = ret_str + '<option value="PURE_HUMAN">Pure Human</option>'
+        ret_str = ret_str + '<option value="RANDOM">Random</option>'
+        ret_str = ret_str + '</select><br>'
+        ret_str = ret_str + '<input type="checkbox" id="sub" value="sub_spec" checked="checked"/>'
+        ret_str = ret_str + 'Assign sub-species for animals and plants<br>'
+        ret_str = ret_str + '<button type="button" onclick="generate_character()">Generate</button><br>'
+        ret_str = ret_str + '</form>'
+        ret_str = ret_str + '<script>'
+        ret_str = ret_str + 'function generate_character() {'
+        ret_str = ret_str + '     window.location = document.getElementById("class_select").value '
+        ret_str = ret_str + '+ "?" + document.getElementById("sub").value + "=" + '
+        ret_str = ret_str + 'document.getElementById("sub").checked;'
+        ret_str = ret_str + '}'
+        ret_str = ret_str + '</script>'
+        ret_str = ret_str + '<a href="/">Home</a>'
+        ret_str = ret_str + '</nav>'
+
+        return ret_str
+
+    def create_head(self):
+        
+        ret_str = '<body><header><h1>Mutant Future Character Generator</h1></header>'
+
+        return ret_str
+
+
+    def create_foot(self):
+
+        ret_str = '<footer>Mutant Future</footer>'
+
+        return ret_str
+
+
 class MainHandler(tornado.web.RequestHandler):
     """Handler for main page of MF char gen"""
 
@@ -13,48 +100,20 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         """respond to HTTP get method"""
 
-        self.write('<html><head><style>')
-        self.create_style_sheet()
-        self.write('<title>Mutant Future Character Generator</title>')
-        self.write('</style></head><body>')
-        self.write('<header><h1>Mutant Future Character Generator</h1></header>')
+        screen_formatter = ScreenFormatter()
+
+        self.write(screen_formatter.create_style_sheet())
+        self.write('<body>')
+        self.write(screen_formatter.create_head())
         self.write('<font size="2">')
-        self.create_nav()
+        self.write(screen_formatter.create_nav())
         self.write('</font>')
         self.write('<font size="2">')
         self.create_body()
         self.write('</font>')
-        self.write('<footer>Mutant Future</footer></body></html>')
+        self.write(screen_formatter.create_foot())
+        self.write('</body></html>')
 
-    def create_style_sheet(self):
-        """create style sheet for page"""
-
-        self.write('header {')
-        self.write('    background-color:blue;')
-        self.write('    color:white;')
-        self.write('    text-align:center;')
-        self.write('    padding:5px;	 ')
-        self.write('}')
-        self.write('nav {')
-        self.write('    line-height:20px;')
-        self.write('    background-color:#eeeeee;')
-        self.write('    float:left;')
-        self.write('    padding:5px;	      ')
-        self.write('}')
-        self.write('body_text {')
-        self.write('    width:800px;')
-        self.write('    float:left;')
-        self.write('    padding:10px;	 	 ')
-        self.write('}')
-        self.write('footer {')
-        self.write('    background-color:blue;')
-        self.write('    color:white;')
-        self.write('    clear:both;')
-        self.write('    text-align:center;')
-        self.write('    padding:5px;	 	 ')
-        self.write('}')
-        self.write('</style>')
-        self.write('<head></head>')
 
     def create_body(self):
         """create body section"""
@@ -81,39 +140,6 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 
-    def create_nav(self):
-        """create navigation section"""
-
-        self.write('<nav>')
-
-        self.write('<b>Class:</b><br>')
-        self.write('<form>')
-        self.write('<select id="class_select">')
-        self.write('<option value="BASIC_ANDROID">Basic Android</option>')
-        self.write('<option value="SYNTHETIC_ANDROID">Synthetic Android</option>')
-        self.write('<option value="REPLICANT">Replicant</option>')
-        self.write('<option value="MUTANT_HUMAN">Mutant Human</option>')
-        self.write('<option value="MUTANT_ANIMAL">Mutant Animal</option>')
-        self.write('<option value="MUTANT_PLANT">Mutant Plant</option>')
-        self.write('<option value="PURE_HUMAN">Pure Human</option>')
-        self.write('<option value="RANDOM">Random</option>')
-        self.write('</select><br>')
-        self.write('<input type="checkbox" id="sub" value="sub_spec" checked="checked"/>' +
-                   'Assign sub-species for animals and plants<br>')
-        self.write('<button type="button" onclick="generate_character()">Generate</button><br>')
-        self.write('</form>')
-
-        self.write('<script>')
-        self.write('function generate_character() {')
-        self.write('     window.location = document.getElementById("class_select").value ' +
-                   '+ "?" + document.getElementById("sub").value + "=" + ' +
-                   'document.getElementById("sub").checked;')
-        self.write('}')
-        self.write('</script>')
-
-        self.write('</nav>')
-
-
 class CharacterHandler(tornado.web.RequestHandler):
     """Handler Purely for Humans"""
 
@@ -122,6 +148,23 @@ class CharacterHandler(tornado.web.RequestHandler):
 
     def get(self):
         """respond to HTTP get method"""
+
+        screen_formatter = ScreenFormatter()
+
+        self.write(screen_formatter.create_style_sheet())
+        self.write('<body>')
+        self.write(screen_formatter.create_head())
+        self.write('<font size="2">')
+        self.write(screen_formatter.create_nav())
+        self.write('</font>')
+        self.write('<font size="2">')
+        self.create_body()
+        self.write('</font>')
+        self.write(screen_formatter.create_foot())
+        self.write('</body></html>')
+
+
+    def create_body(self):
 
         sub_spec = False
 
@@ -188,7 +231,6 @@ class CharacterHandler(tornado.web.RequestHandler):
         for modifier in character['modifiers']:
             self.write(modifier + '<br>')
         self.write('<br>')
-        self.write('<a href="/">Back</a> ')
 
 
 
