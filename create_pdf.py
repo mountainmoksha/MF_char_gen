539f1ed627e4a08e8362a84c81ade49a70671848
David T. MacKenzie <Exit 0>
2016-08-21 18:28:28 -0600

    added missile mod to character and pdf generator

--- create_pdf.py
+++ create_pdf.py
@@ -58,6 +58,7 @@ def gen_char_pdf(character):
     character_canvas.drawString(222, 538, str(character['str_mod']))
 
     character_canvas.drawString(222, 498, str(character['ac_mod']))
+    character_canvas.drawString(292, 498, str(character['missile_mod']))
 
     character_canvas.showPage()
     character_canvas.save()
