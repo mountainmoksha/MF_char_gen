47c2c6cd128f0b6a5166a7712683660ec5935b90
David T. MacKenzie <Exit 0>
2016-10-16 18:31:03 -0600

    More comprehensive unittests to gen_char

--- tests/test_gen_char.py
+++ tests/test_gen_char.py
@@ -17,13 +17,41 @@ class SimpleTestCase(unittest.TestCase):
         """Call after every test case."""
         os.chdir(self.tests_path)
 
+    def test_char_name(self):
+
+        character = gen_char.char()
+
+        self.assertTrue(isinstance(gen_char.char_name(character), str))
+
     def test_char(self):
         """test the proper operation of character generator"""
 
         character = gen_char.char()
-        #TODO: do this
-        print('this test in not complete')
 
+        self.assertTrue('missile_mod' in character)
+        self.assertTrue('rad_mod' in character)
+        self.assertTrue('HP' in character)
+        self.assertTrue('level' in character)
+        self.assertTrue('name' in character)
+        self.assertTrue('reaction_mod' in character)
+        self.assertTrue('stun_save' in character)
+        self.assertTrue('modifiers' in character)
+        self.assertTrue('AC' in character)
+        self.assertTrue('plant' in character)
+        self.assertTrue('type' in character)
+        self.assertTrue('poison_mod' in character)
+        self.assertTrue('physical' in character)
+        self.assertTrue('ac_mod' in character)
+        self.assertTrue('energy_save' in character)
+        self.assertTrue('XP' in character)
+        self.assertTrue('GP' in character)
+        self.assertTrue('rad_save' in character)
+        self.assertTrue('mental' in character)
+        self.assertTrue('level_modifiers' in character)
+        self.assertTrue('attributes' in character)
+        self.assertTrue('poison_death_save' in character)
+        self.assertTrue('str_mod' in character)
+        self.assertTrue('tech_mod' in character)
 
 
 if __name__ == "__main__":
