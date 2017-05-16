7cdcea373dfabb6d14cac0eb698cf0d144bf6418
David T. MacKenzie <Exit 0>
2017-05-15 20:55:06 -0600

    connect up and pre-check more screen controls

--- TRUNK/tornado_mf.py
+++ TRUNK/tornado_mf.py
@@ -423,7 +423,8 @@ class PDFViewHandler(tornado.web.RequestHandler):
                 else:
                     level_select = int(section.split('=')[1])
 
-        character = gen_char.char(class_select)
+        character = gen_char.char(class_select, level_select, sub_spec,
+                                  assign_name, rand_synth, rand_repl)
 
         file_name = create_pdf.gen_char_pdf(character)
 
