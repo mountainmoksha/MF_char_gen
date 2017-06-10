6e3ce7684684d6d5c3fd2d11f873fe4e9fd4a694
David T. MacKenzie <Exit 0>
2017-06-10 07:06:54 -0600

    update db module

--- TRUNK/db_access.py
+++ TRUNK/db_access.py
@@ -2,6 +2,7 @@
 """db access module for mfchargen"""
 
 import tinydb
+from tinydb.operations import delete
 import gen_char
 
 def query_by_name(name=''):
@@ -9,8 +10,12 @@ def query_by_name(name=''):
 
     db = tinydb.TinyDB('chars.json')
     char_query = tinydb.Query()
-    this_char = db.search(char_query.name == name)[0]
+    try:
+        this_char = db.search(char_query.name == name)[0]
+    except IndexError:
+        this_char = None
     db.close()
+    return this_char
 
 def insert_char(character):
     """insert a character dictionary into db"""
@@ -19,6 +24,16 @@ def insert_char(character):
     db.insert(character)
     db.close()
 
+def rm_by_name(name):
+    db = tinydb.TinyDB('chars.json')
+    char_query = tinydb.Query()
+    db.remove(char_query.name == name)
+    db.close()
+
 if __name__ == '__main__':
 
     CHARACTER = gen_char.char()
+    insert_char(CHARACTER)
+    RET_CHAR = query_by_name(CHARACTER['name'])
+    print(RET_CHAR)
+    rm_by_name(CHARACTER['name'])
