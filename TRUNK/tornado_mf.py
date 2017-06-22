2a7fa0467362056b697f61a37a4279d072a66489
David T. MacKenzie <Exit 0>
2017-06-21 18:50:54 -0600

    minimally connect edit screen to pdf display

--- TRUNK/tornado_mf.py
+++ TRUNK/tornado_mf.py
@@ -46,14 +46,34 @@ class CommitHandler(tornado.web.RequestHandler):
             this_section = section.split('=')
             if this_section[0] == 'name':
                 name = section.split('=')[1]
-
-        #TODO: pull attrs out of url and commit
+            if this_section[0] == 'Strength':
+                strength = section.split('=')[1]
+            if this_section[0] == 'Dexterity':
+                dexterity = section.split('=')[1]
+            if this_section[0] == 'Constitution':
+                constitution = section.split('=')[1]
+            if this_section[0] == 'Intelligence':
+                intelligence = section.split('=')[1]
+            if this_section[0] == 'Willpower':
+                willpower = section.split('=')[1]
+            if this_section[0] == 'Charisma':
+                charisma = section.split('=')[1]
 
         name = name.replace('%20', ' ')
 
         character = db_access.query_by_name(name)
 
-        name.replace(' ', '%20')
+        (character['attributes'])['Strength'] = strength
+        (character['attributes'])['Dexterity'] = dexterity
+        (character['attributes'])['Constitution'] = constitution
+        (character['attributes'])['Intelligence'] = intelligence
+        (character['attributes'])['Willpower'] = willpower
+        (character['attributes'])['Charisma'] = charisma
+
+        db_access.rm_by_name(character['name'])
+        db_access.insert_char(character)
+
+        name = name.replace(' ', '%20')
 
         self.write('<head>')
         self.write('<meta http-equiv="refresh" content="0; url=/VIEW_PDF?name=' + name + '" />')
@@ -67,6 +87,30 @@ class EditHandler(tornado.web.RequestHandler):
 
     def data_received(self, chunk):
         print('chunk is', chunk)
+
+    def attr_dropdown(self, character, attr='Strength'):
+        """generate drop-down for discreet attribute"""
+
+        ret_str = '<label class="title">' + attr + '</label>'
+        ret_str += '<div class="large">'
+        ret_str += '<span>'
+        ret_str += '<select name="' + attr.lower() + '" id="' + attr.lower() + '_select">'
+        score = int((character['attributes'])[attr])
+        for attr_idx in range(3,18):
+            if score == attr_idx:
+                out_line = '<option value="' + str(attr_idx) + '" selected>'
+                out_line += str(attr_idx) + '</option>'
+                ret_str += out_line
+            else:
+                out_line = '<option value="' + str(attr_idx) + '">'
+                out_line += str(attr_idx) + '</option>'
+                ret_str += out_line
+        ret_str += '</span>'
+        ret_str += '</select>'
+        ret_str += '</div>'
+        ret_str += '<br>'
+
+        return ret_str
 
     def get(self):
         """respond to HTTP get method"""
@@ -139,116 +183,24 @@ class EditHandler(tornado.web.RequestHandler):
         self.write('<div class="title">')
         self.write('<h2>Mutant Future Character Generator</h2>')
         self.write('</div>')
+
         self.write('<div class="element-select">')
-        self.write('<label class="title">Strength</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="strength" id="strength_select">')
 
-        str_score = int((character['attributes'])['Strength'])
-        for attr_idx in range(3,18):
-            if str_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
-
-        self.write('<label class="title">Dexterity</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="dexterity" id="dexterity_select">')
-        dex_score = int((character['attributes'])['Dexterity'])
-        for attr_idx in range(3,18):
-            if dex_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
-
-        self.write('<label class="title">Constitution</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="constitution" id="constitution_select">')
-        con_score = int((character['attributes'])['Constitution'])
-        for attr_idx in range(3,18):
-            if con_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
-
-        self.write('<label class="title">Intelligence</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="intelligence" id="intelligence_select">')
-        int_score = int((character['attributes'])['Intelligence'])
-        for attr_idx in range(3,18):
-            if int_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
-
-        self.write('<label class="title">Willpower</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="willpower" id="willpower_select">')
-        wil_score = int((character['attributes'])['Willpower'])
-        for attr_idx in range(3,18):
-            if wil_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
-
-        self.write('<label class="title">Charisma</label>')
-        self.write('<div class="large">')
-        self.write('<span>')
-        self.write('<select name="charisma" id="charisma_select">')
-        cha_score = int((character['attributes'])['Charisma'])
-        for attr_idx in range(3,18):
-            if cha_score == attr_idx:
-                out_line = '<option value="' + str(attr_idx) + '" selected>'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-            else:
-                out_line = '<option value="' + str(attr_idx) + '">'
-                out_line += str(attr_idx) + '</option>'
-                self.write(out_line)
-        self.write('</select>')
+        for attr in character['attributes']:
+            self.write(self.attr_dropdown(character, attr))
 
         self.write('<i></i></span></div></div><br>')
-#        self.write('<div class="submit"><input type="submit" ')
-#        self.write('value="View PDF" onclick="view_pdf()"></div>')
         self.write('<div><button type="button" onclick="view_pdf()" ')
         self.write('style="background-color:red;">VIEW PDF</button></div>')
         self.write('<br><img src="MF_logo_color.png" alt="MF_logo_color.png" width="100%">')
         self.write('</form>\n')
         self.write('<script>\n')
         self.write('function view_pdf() {\n')
-        self.write('view_url = \"COMMIT_CHAR?name=')
-        self.write(character['name'] + '\"\n')
-        #TODO: put other attrs into new url
+        self.write('view_url = \"COMMIT_CHAR?name=' + character['name'].replace(' ', '%20') + '\"\n')
+        for attr in character['attributes']:
+            self.write('view_url += \"?' + attr + '=\"\n')
+            self.write(str('view_url += document.getElementById("' + attr.lower() +
+                           '_select").value' + '\n'))
         self.write('window.open(view_url)\n')
         self.write('}\n')
         self.write('</script>\n')
