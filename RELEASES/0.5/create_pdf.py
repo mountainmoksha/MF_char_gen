b129542283ce491ff9390fe6dff3a20584419402
David T. MacKenzie <Exit 0>
2017-06-26 19:44:49 -0600

    0.5 release!

---
+++ RELEASES/0.5/create_pdf.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+"""take a character dict, and make a filled-in pdf character sheet from it"""
+
+import os
+from reportlab.pdfgen import canvas
+from pdfrw import PageMerge, PdfReader, PdfWriter
+import xml.dom.minidom
+import gen_char
+
+
+def combine_pdfs(character, overlay_file_name):
+    """add the previously-created, filled pdf to
+       Goblinoid games's stock sheet"""
+
+    total_pages = 2
+    if (character['plant'])[0] != '':
+        total_pages += 1
+    if (character['physical'])[0] != '':
+        total_pages += 1
+    if (character['mental'])[0] != '':
+        total_pages += 1
+
+    base_file_name = 'pdf/mfcharsheet_' + str(total_pages) + '_page.pdf'
+    final_file_name = overlay_file_name.replace('_blank', '')
+
+    trailer = PdfReader(base_file_name)
+
+    page_idx = 0
+
+    for page in trailer.pages:
+
+        overlay = PageMerge().add(PdfReader(overlay_file_name).pages[page_idx])[0]
+        PageMerge(page).add(overlay).render()
+
+        page_idx = page_idx + 1
+
+    PdfWriter().write(final_file_name, trailer)
+
+    os.remove(overlay_file_name)
+
+    return final_file_name
+
+def gen_char_pdf(character):
+    """put the numbers and words from a character on a "blank" sheet
+       to be added to the stock sheet"""
+
+    file_name = 'mutations_descriptions.xml'
+    xml_p = open(file_name, 'r')
+    xml_string = xml_p.read()
+    xml_p.close()
+    doc_obj = xml.dom.minidom.parseString(xml_string)
+
+    file_name = 'char_pdfs/' + character['name'].replace(' ', '_') + '_blank.pdf'
+
+    character_canvas = canvas.Canvas(file_name)
+    character_canvas.setTitle(character['name'])
+    character_canvas.setFont('Courier-BoldOblique', 10)
+    character_canvas.setFillColorRGB(1.0, 0.0, 0.0)
+
+    character_canvas.drawString(77, 528, str((character['attributes'])['Strength']))
+    character_canvas.drawString(77, 488, str((character['attributes'])['Dexterity']))
+    character_canvas.drawString(77, 446, str((character['attributes'])['Constitution']))
+    character_canvas.drawString(77, 402, str((character['attributes'])['Intelligence']))
+    character_canvas.drawString(77, 360, str((character['attributes'])['Willpower']))
+    character_canvas.drawString(77, 318, str((character['attributes'])['Charisma']))
+
+    if character['use-name']:
+        character_canvas.drawString(246, 683, str(character['name']))
+
+    if 'sub_type' in character:
+        character_canvas.drawString(456, 697, str(character['type']))
+        character_canvas.drawString(456, 686, '(' + str(character['sub_type']) + ')')
+    else:
+        character_canvas.drawString(456, 686, str(character['type']))
+
+    character_canvas.drawString(373, 647, str(character['level']))
+    character_canvas.drawString(480, 647, str(character['XP']))
+    character_canvas.drawString(415, 575, str(character['HP']))
+    character_canvas.drawString(513, 575, str(character['AC']))
+
+    character_canvas.drawString(409, 483, str(character['energy_save']))
+    character_canvas.drawString(409, 439, str(character['poison_death_save']))
+    character_canvas.drawString(409, 393, str(character['stun_save']))
+    character_canvas.drawString(409, 347, str(character['rad_save']))
+
+    character_canvas.drawString(222, 538, str(character['str_mod']))
+
+    character_canvas.drawString(222, 498, str(character['ac_mod']))
+    character_canvas.drawString(292, 498, str(character['missile_mod']))
+
+    character_canvas.drawString(222, 455, str(character['poison_mod']))
+    character_canvas.drawString(292, 455, str(character['rad_mod']))
+
+    character_canvas.drawString(217, 410, str(character['tech_mod']))
+
+    character_canvas.drawString(221, 330, str(character['reaction_mod']))
+
+    mutations_current_height = 250
+
+    if 'level_modifiers' in character:
+        character_canvas.drawString(75, mutations_current_height, 'Level Modifiers:')
+        mutations_current_height = mutations_current_height - 13
+        for level_mod_key in character['level_modifiers']:
+            character_canvas.drawString(85, mutations_current_height, (character['level_modifiers'])[level_mod_key])
+            mutations_current_height = mutations_current_height - 13
+
+    # this advances us to page 2:
+    character_canvas.showPage()
+    character_canvas.setFont('Courier-BoldOblique', 8)
+#    print(character_canvas.getAvailableFonts())
+    character_canvas.setFillColorRGB(0.0, 0.0, 1.0)
+
+    # top of page, on these sheets
+    mutations_current_height = 670
+
+    if (character['plant'])[0] != '':
+        character_canvas.drawString(75, mutations_current_height, 'Plant Mutations:')
+        mutations_current_height -= 9
+        for mutation in character['plant']:
+            character_canvas.drawString(85, mutations_current_height, mutation)
+            mutations_current_height -= 9
+            # put in description
+            text = doc_obj.getElementsByTagName(mutation.replace(' ', '_'))
+            try:
+                lines = text[0].firstChild.nodeValue
+                for line in lines.split('\n'):
+                    character_canvas.drawString(95, mutations_current_height, line)
+                    mutations_current_height -= 9
+            except IndexError:
+                print(mutation.replace(' ', '_'), 'not found')
+        # this advances us to the next page
+        character_canvas.showPage()
+        character_canvas.setFont('Courier-BoldOblique', 8)
+        character_canvas.setFillColorRGB(0.0, 0.0, 1.0)
+
+    # top of page, on these sheets
+    mutations_current_height = 670
+
+    if (character['physical'])[0] != '':
+        character_canvas.drawString(75, mutations_current_height, 'Physical Mutations:')
+        mutations_current_height -= 9
+        for mutation in character['physical']:
+            character_canvas.drawString(85, mutations_current_height, mutation)
+            mutations_current_height -= 9
+            # put in description
+            text = doc_obj.getElementsByTagName(mutation.replace(' ', '_'))
+            try:
+                lines = text[0].firstChild.nodeValue
+                for line in lines.split('\n'):
+                    character_canvas.drawString(95, mutations_current_height, line)
+                    mutations_current_height -= 9
+            except IndexError:
+                print(mutation.replace(' ', '_'), 'not found')
+        # this advances us to the next page
+        character_canvas.showPage()
+        character_canvas.setFont('Courier-BoldOblique', 8)
+        character_canvas.setFillColorRGB(0.0, 0.0, 1.0)
+
+    # top of page, on these sheets
+    mutations_current_height = 670
+
+    if (character['mental'])[0] != '':
+        character_canvas.drawString(75, mutations_current_height, 'Mental Mutations:')
+        mutations_current_height -= 9
+        for mutation in character['mental']:
+            character_canvas.drawString(85, mutations_current_height, mutation)
+            mutations_current_height -= 9
+            # put in description
+            text = doc_obj.getElementsByTagName(mutation.replace(' ', '_'))
+            try:
+                lines = text[0].firstChild.nodeValue
+                for line in lines.split('\n'):
+                    character_canvas.drawString(95, mutations_current_height, line)
+                    mutations_current_height -= 9
+            except IndexError:
+                print(mutation.replace(' ', '_'), 'not found')
+        # this advances us to the next page
+        character_canvas.showPage()
+        character_canvas.setFont('Courier-BoldOblique', 8)
+        character_canvas.setFillColorRGB(0.5, 0.0, 0.5)
+
+    # do this (potentially again) here, in case there
+    # weren't any mutations
+    character_canvas.setFont('Courier-BoldOblique', 10)
+    character_canvas.setFillColorRGB(0.5, 0.0, 0.5)
+
+    character_canvas.drawString(360, 270, str(character['GP']) + ' GP')
+
+    character_canvas.save()
+
+    return file_name
+
+if __name__ == '__main__':
+
+    CHARACTER = gen_char.char()
+
+    FILE_NAME = gen_char_pdf(CHARACTER)
+
+    print(combine_pdfs(CHARACTER, FILE_NAME))
---
+++ RELEASES/0.5/gen_char.py
@@ -0,0 +1,363 @@
+#!/usr/bin/env python3
+"""main module: Mutant Future low-level char gen"""
+
+import datetime
+import random
+import read_muts
+import gen_attrs
+import gen_mods
+
+def gen_xp(character):
+    """assign minimum XP to character for given level"""
+
+    character['XP'] = 0
+
+    if character['level'] == 2:
+        character['XP'] = 3001
+    elif character['level'] == 3:
+        character['XP'] = 6001
+    elif character['level'] == 4:
+        character['XP'] = 12001
+    elif character['level'] == 5:
+        character['XP'] = 24001
+    elif character['level'] == 6:
+        character['XP'] = 48001
+    elif character['level'] == 7:
+        character['XP'] = 96001
+    elif character['level'] == 8:
+        character['XP'] = 192001
+    elif character['level'] == 9:
+        character['XP'] = 492001
+    elif character['level'] == 10:
+        character['XP'] = 892001
+    elif character['level'] == 11:
+        character['XP'] = 1392001
+    elif character['level'] == 12:
+        character['XP'] = 2392001
+
+
+
+def gen_saves(character):
+    """produce modified saving throws by level"""
+
+    if character['level'] < 1:
+        character['energy_save'] = 17
+        character['poison_death_save'] = 14
+        character['stun_save'] = 16
+        character['rad_save'] = 15
+    elif character['level'] < 4:
+        character['energy_save'] = 15
+        character['poison_death_save'] = 12
+        character['stun_save'] = 14
+        character['rad_save'] = 13
+    elif character['level'] < 7:
+        character['energy_save'] = 13
+        character['poison_death_save'] = 10
+        character['stun_save'] = 12
+        character['rad_save'] = 11
+    elif character['level'] < 10:
+        character['energy_save'] = 9
+        character['poison_death_save'] = 8
+        character['stun_save'] = 10
+        character['rad_save'] = 9
+    elif character['level'] < 13:
+        character['energy_save'] = 7
+        character['poison_death_save'] = 6
+        character['stun_save'] = 8
+        character['rad_save'] = 7
+    elif character['level'] < 16:
+        character['energy_save'] = 5
+        character['poison_death_save'] = 4
+        character['stun_save'] = 6
+        character['rad_save'] = 5
+    elif character['level'] < 19:
+        character['energy_save'] = 4
+        character['poison_death_save'] = 4
+        character['stun_save'] = 5
+        character['rad_save'] = 4
+    else:
+        character['energy_save'] = 4
+        character['poison_death_save'] = 3
+        character['stun_save'] = 4
+        character['rad_save'] = 3
+
+    character['poison_death_save'] = character['poison_death_save'] + int(character['poison_mod'])
+    character['rad_save'] = character['rad_save'] + int(character['rad_mod'])
+
+
+def gen_level_mods(character):
+    """custom leveling system.  MF rules p. 14"""
+
+    level_modifiers = {}
+
+    for level_idx in range(2, character['level']+1):
+
+        level_roll = random.randrange(100)
+
+        if ((level_roll > 0) and
+            (level_roll < 11)):
+            level_modifiers['level' + str(level_idx)] = str('+1 melee damage from level ' +
+                                                            str(level_idx) + ' advancement')
+        elif ((level_roll > 10) and
+            (level_roll < 21)):
+            level_modifiers['level' + str(level_idx)] = str('+1 attack/round from level ' +
+                                                            str(level_idx) + ' advancement')
+        else:
+            ability_roll = random.randrange(1, 7)
+            if ability_roll == 1:
+                strn = (character['attributes'])['Strength']
+                strn = strn + 1
+                (character['attributes'])['Strength'] = strn
+            elif ability_roll == 2:
+                dex = (character['attributes'])['Dexterity']
+                dex = dex + 1
+                (character['attributes'])['Dexterity'] = dex
+            elif ability_roll == 3:
+                con = (character['attributes'])['Constitution']
+                con = con + 1
+                (character['attributes'])['Constitution'] = con
+            elif ability_roll == 4:
+                intel = (character['attributes'])['Intelligence']
+                intel = intel + 1
+                (character['attributes'])['Intelligence'] = intel
+            elif ability_roll == 5:
+                wil = (character['attributes'])['Willpower']
+                wil = wil + 1
+                (character['attributes'])['Willpower'] = wil
+            elif ability_roll == 6:
+                char = (character['attributes'])['Charisma']
+                char = char + 1
+                (character['attributes'])['Charisma'] = char
+
+    character['level_modifiers'] = level_modifiers
+
+
+def char_mutations(character):
+    """assign physical, mental and plant mutations"""
+
+    # Gen Mutations variably by class
+    if ((character['type'] == 'Mutant Human') or
+            (character['type'] == 'Mutant Animal')):
+        character['physical'] = read_muts.muts('physical')
+        character['mental'] = read_muts.muts('mental')
+        character['plant'] = ['']
+
+    elif character['type'] == 'Mutant Plant':
+        character['plant'] = read_muts.muts('plant', 2)
+        character['physical'] = []
+        character['mental'] = []
+        for rand_idx in range(random.randrange(6) + 1):
+            if (rand_idx % 2) == 0:
+                phys_mut = read_muts.muts('physical')[0]
+                while phys_mut in character['physical']:
+                    phys_mut = read_muts.muts('physical')[0]
+                character['physical'].append(phys_mut)
+            else:
+                phys_mut = read_muts.muts('mental')[0]
+                while phys_mut in character['mental']:
+                    phys_mut = read_muts.muts('mental')[0]
+                character['mental'].append(phys_mut)
+        if len(character['physical']) == 0:
+            character['physical'].append('')
+        if len(character['mental']) == 0:
+            character['mental'].append('')
+
+    elif character['type'] == 'Pure Human':
+        character['physical'] = ['']
+        character['mental'] = ['']
+        character['plant'] = ['']
+
+    else: # androids
+        character['physical'] = ['Pick any combination of 3 beneficial ' +
+                                 'mental and/or physical mutations']
+        character['mental'] = ['Pick any combination of 3 beneficial ' +
+                               'mental and/or physical mutations']
+        character['plant'] = ['']
+
+
+def char_hit_points(character):
+    """obtain character hit points by class"""
+
+    # Gen HP by class
+    hit_points = 0
+    if character['type'] == 'Pure Human':
+        for _ in range((character['attributes'])['Constitution']):
+            hit_points = hit_points + random.randrange(1, 9)
+        character['HP'] = hit_points
+
+    elif character['type'] == 'Mutant Human':
+        for _ in range((character['attributes'])['Constitution']):
+            hit_points = hit_points + random.randrange(1, 7)
+        character['HP'] = hit_points
+
+    elif character['type'] == 'Mutant Animal':
+        for _ in range((character['attributes'])['Constitution']):
+            hit_points = hit_points + random.randrange(1, 7)
+        character['HP'] = hit_points
+
+    elif character['type'] == 'Mutant Plant':
+        for _ in range((character['attributes'])['Constitution']):
+            hit_points = hit_points + random.randrange(1, 7)
+        character['HP'] = hit_points
+
+    else: # androids
+        character['HP'] = 50
+
+
+def char_name(character):
+    """I suspect this will be a little controversal with
+       people.  It's a completely randomized naming system"""
+
+    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+                  'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    character_name = ''
+
+    if ((character['type'] == 'Pure Human') or
+        (character['type'] == 'Mutant Human')):
+
+        # first name:
+        for letter_idx in range(random.randrange(3, 6)):
+            if letter_idx % 2 == 0:
+                letter = consonants[random.randrange(len(consonants))]
+            else:
+                letter = vowels[random.randrange(len(vowels))]
+
+            if letter_idx == 0:
+                character_name = character_name + letter.upper()
+            else:
+                character_name = character_name + letter.lower()
+
+        character_name = character_name + ' '
+
+        # last name:
+        for letter_idx in range(random.randrange(4, 8)):
+            if letter_idx % 2 == 0:
+                letter = consonants[random.randrange(len(consonants))]
+            else:
+                letter = vowels[random.randrange(len(vowels))]
+
+            if letter_idx == 0:
+                character_name = character_name + letter.upper()
+            else:
+                character_name = character_name + letter.lower()
+
+    elif character['type'] == 'Mutant Animal':
+
+        # one name, but longer
+        for letter_idx in range(random.randrange(5, 8)):
+            if letter_idx % 2 == 0:
+                letter = vowels[random.randrange(len(vowels))]
+            else:
+                letter = consonants[random.randrange(len(consonants))]
+
+            if letter_idx == 0:
+                character_name = character_name + letter.upper()
+            else:
+                character_name = character_name + letter.lower()
+
+    elif character['type'] == 'Mutant Plant':
+
+        # one name, but longer
+        for letter_idx in range(random.randrange(5, 8)):
+            letter = vowels[random.randrange(len(vowels))]
+
+            if letter_idx == 0:
+                character_name = character_name + letter.upper()
+            else:
+                character_name = character_name + letter.lower()
+
+    else: # all androids
+
+        character_name = consonants[random.randrange(len(consonants))].upper() + '-'
+
+        for _ in range(5):
+
+            character_name = character_name + str(random.randrange(10))
+
+        character_name = character_name + '-' + vowels[random.randrange(len(vowels))].upper()
+
+
+    return character_name
+
+
+def char(char_type=None, level=1, sub_type=True, gen_name=True, rand_synth=False,
+         rand_repl=False, method='3d6'):
+    """obtain fully-populated character"""
+
+    with open('MF_classes.txt', 'r') as classes_file:
+        types = classes_file.read().splitlines()
+
+    if char_type is None:
+
+        if not rand_synth:
+            types.remove('Synthetic Android')
+
+        if not rand_repl:
+            types.remove('Replicant')
+
+        char_type = types[random.randrange(len(types))]
+
+    if char_type not in types:
+        print('invalid type')
+        exit(1)
+
+    # Gen basics of all characters
+    character = {}
+    character['type'] = char_type
+
+    character['name'] = char_name(character)
+    if gen_name:
+        character['use-name'] = True
+    else:
+        character['use-name'] = False
+
+    character['attributes'] = gen_attrs.attrs(method)
+
+    if level is None:
+        character['level'] = random.randrange(1, 11)
+    else:
+        character['level'] = level
+
+    gen_level_mods(character)
+
+    # optional sub-species for plants and animals
+    if sub_type:
+
+        if character['type'] == 'Mutant Animal':
+            with open('MF_animals.txt', 'r') as animals_file:
+                animals = animals_file.read().splitlines()
+            character['sub_type'] = animals[random.randrange(len(animals))]
+
+        if character['type'] == 'Mutant Plant':
+            with open('MF_plants.txt', 'r') as plants_file:
+                plants = plants_file.read().splitlines()
+            character['sub_type'] = plants[random.randrange(len(plants))]
+
+    # Base unarmored AC
+    character['AC'] = 9
+
+    char_hit_points(character)
+
+    character['GP'] = (random.randrange(1, 9) +
+                       random.randrange(1, 9) +
+                       random.randrange(1, 9)) * 10
+
+    char_mutations(character)
+
+    gen_mods.gen_modifiers(character)
+
+    gen_saves(character)
+
+    gen_xp(character)
+
+    character['creation_time'] = str(datetime.datetime.utcnow())
+
+    return character
+
+if __name__ == '__main__':
+
+    CHARACTER = char(None, None, True, True, False, False)
+    print(CHARACTER)
